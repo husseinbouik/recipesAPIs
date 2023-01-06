06118879f52b1a1f6746612495f4cec15c29fb0e
husseinbouik <116066389+husseinbouik@users.noreply.github.com>
2023-01-06 16:54:01 +0100

    bruuuh

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
+// get element by id 
+const cards= document.getElementById("allcards") 
+// declare avaribal to apend our html 
+let output=""
+// after the load of content show us  6 random cards from the API(url)puis get the as a parametre response and returne it type json ,from the json data creats the cards 
+ window.addEventListener("DOMContentLoaded", function () {
+  for (let i = 0; i < 6; i++) {
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => creatcards(json.meals[0]))
+      
+  }
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-card +=`<div class="card mb-3" style="max-width: 540px;">
+  function creatcards(data)
+   {
+    output+=`<div class="card mb-3" style="max-width: 540px;">
 <div class="row g-0">
   <div class="col-md-4">
-    <img src="${}" class="img-fluid rounded-start" alt="...">
+    <img src="${data.strMealThumb}" class="img-fluid rounded-start" alt="...">
   </div>
   <div class="col-md-8">
     <div class="card-body">
-      <h5 class="card-title">${}</h5>
+      <h5 class="card-title">${data.strMeal}</h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <a href="#" class="btn btn-primary" onclick="modaldata(${data.idMeal})"    data-bs-toggle="modal"  data-bs-target="#exampleModal">now MORE DETAILS</a>
     </div>
   </div>
 </div>
 </div>`
+
+  console.log(output);
+  cards.innerHTML=output
+   }
